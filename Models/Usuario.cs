caf2fbe9563ae348486b4fb281ff5a1e29dd61a3
Felipe Braghittoni <100584090+Feezones@users.noreply.github.com>
2022-12-13 12:19:05 -0300

    first comit
    api e banco

---
+++ Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teste.Objetos
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfSenha { get; set; }
+    }
+}
---
+++ Models/UsuarioContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using teste.Objetos;
+
+namespace teste.Models
+{
+    public class UsuarioContext : DbContext
+    {
+
+        public DbSet<Usuario> Users { get; set; } = null!;
+
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+       : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=D:\Repositorio\teste\LoginsDb.db");
+    }
+}
