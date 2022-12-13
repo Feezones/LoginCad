cc9dea36bc055603e61b41f4ee6335cf4e6c7905
Felipe Braghittoni <100584090+Feezones@users.noreply.github.com>
2022-12-13 17:56:52 -0300

    mais att
    n mudo nada desde a ultima att

--- Front/telaCad/script.js
+++ Front/telaCad/script.js
@@ -2,56 +2,64 @@
 function main(){
     fetch('https://localhost:7041/api/Usuarios',
     {
-        method: 'POST',
+        method: 'GET',
     })
     .then(response => response.json())
     .then(data => console.log('values', data))
     .catch(error => console.log('error', error));
 }
-
 main()
 
 
+
 function sendForm(){
-    formulario=[
      nome=document.getElementById("nome").value,
      sobrenome=document.getElementById("sobrenome").value,
      CPF=document.getElementById("CPF").value,
      endereco=document.getElementById("endereco").value,
+     telefone=document.getElementById("telefone").value.
      email=document.getElementById("email").value,
      senha=document.getElementById("senha").value,
      confSenha=document.getElementById("confSenha").value
-    ]
 
-    const formJSON={
-        "nome":formulario[0],
-        "sobrenome":formulario[1],
-        "cpf":formulario[2],
-        "endereco":formulario[3],
-        "email":formulario[4],
-        "senha":formulario[5],
-        "confSenha":formulario[6],
+
+      formJSON2={
+        "nome":String(nome),
+        "sobrenome":String(sobrenome),
+        "cpf":String(CPF),
+        "endereco":String(endereco),
+        "telefone":String(telefone),
+        "email":String(email),
+        "senha":String(senha),
+        "confSenha":String(confSenha),
     }
 
-    console.log(formJSON)
+     formJSON={
+        "nome":'felipe',
+        "sobrenome":'brag',
+        "cpf":'14514514512',
+        "endereco":'rua de casa',
+        "telefone":'145145124',
+        "email":'email@gmail.com',
+        "senha":'senhaforte123',
+        "confSenha":'senhaforte123',
+    }
 
+    console.log(formJSON,formJSON2)
+}
+
+/*
 fetch('https://localhost:7041/api/Usuarios',{
     method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
     headers: {
-        'Content-Type':'application/json'
+        'Content-Type':'application/json;'
     }, 
-    body: JSON.stringify(formJSON)
+    body: JSON.stringify()
 })
 .then(response => response.json())
 .then(data => console.log('Sucesso', data))
 .catch(error => console.log('Error', error));
-}
-
-
-
+*/
 
 
 
