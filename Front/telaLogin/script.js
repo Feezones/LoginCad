66d5d6c3d495fb2be341e7314f43bff5f9e448a6
Felipe Braghittoni <100584090+Feezones@users.noreply.github.com>
2022-12-14 12:38:26 -0300

    att
    tela de login funcionando, talvez precise fazer alguns ajustes futuramente

--- Front/telaLogin/script.js
+++ Front/telaLogin/script.js
@@ -1,19 +1,46 @@
-function logar(){
-
-
-
+function logarAPI(){
+    
     fetch('https://localhost:7041/api/Usuarios',{
-    method: 'POST',
-    headers: {
-        'Content-Type':'application/json;'
-    }, 
-    body: JSON.stringify(formJSON2)
+    method: 'GET',
 })
-.then(response => response.json())
-.then(data => console.log('Sucesso', data))
-.catch(error => console.log('Error', error));
 
+.then(response => response.json())
+.then(data => console.log('Sucesso',data,validaLogin(data)))
+.catch(error => console.log('Error', error));
 }
+
+
+
+function validaLogin(logins){
+    var email = document.getElementById('email').value
+    var senha = document.getElementById('senha').value
+
+    if(email != null && email != ''){
+        //console.log(email)
+        //console.log(senha)
+        i=0
+        while(i<logins.length){
+            if(email == logins[i].email && senha == logins[i].senha){ 
+                //console.log(logins[i].email,logins[i].senha)
+                //console.log('email correto')
+                //console.log('senha correta')
+                //console.log('vc esta logado')
+                result=(`bem vindo ${logins[i].nome}`)
+
+
+                i=logins.length   
+            }else{
+                i++
+                result='dados incorretos'
+            }
+        }
+        alert(result)
+    }else{
+        alert('preencha os dados com email e senha')
+    }
+}
+
+
 
 function toCad(){
     window.location.href = "../telaCad/cadastro.html"
